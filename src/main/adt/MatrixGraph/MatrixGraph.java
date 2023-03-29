c671b0b05455050830f4839d30e72db5971a03bc
MateoGiraz <mateogiraz27@gmail.com>
2023-03-29 12:27:19 -0300

    added tests

---
+++ src/main/adt/MatrixGraph/MatrixGraph.java
@@ -0,0 +1,91 @@
+package adt.MatrixGraph;
+
+import adt.Edge;
+import adt.Interfaces.Graph;
+import adt.Interfaces.List;
+import adt.LinkedGraph.EdgeLinkedList;
+
+public class MatrixGraph implements Graph {
+  int[][] mat;
+  int edgeCount;
+  int[] vertexEdgeCount;
+
+  public MatrixGraph(int elems) {
+    edgeCount = 0;
+    mat = new int[elems][elems];
+    vertexEdgeCount = new int[elems];
+  }
+
+  @Override
+  public int size() {
+    return mat.length;
+  }
+
+  @Override
+  public void addEdge(int v, int w, int weight) {
+    if (isOutOfRange(v, w))
+      return;
+
+    if (!isDefined(v, w)) {
+      edgeCount++;
+      vertexEdgeCount[v]++;
+    }
+
+    mat[v][w] = weight;
+  }
+
+  private boolean isOutOfRange(int v, int w) {
+    return v < 0 || w < 0 || v >= mat.length || w >= mat.length;
+  }
+
+  @Override
+  public int edgeCount(int v) {
+    return vertexEdgeCount[v];
+  }
+
+  @Override
+  public int edgeCount() {
+    return edgeCount;
+  }
+
+  @Override
+  public Iterable<Edge> edges(int v) {
+    List<Edge> list = new EdgeLinkedList<Edge>();
+
+    for (int i = 0; i < mat.length; i++) {
+      if (isDefined(v, i)) {
+        list.addElement(new Edge(v, i, mat[v][i]));
+      }
+    }
+
+    return (Iterable<Edge>) list;
+  }
+
+  private boolean isDefined(int v, int i) {
+    return mat[v][i] != 0;
+  }
+
+  @Override
+  public boolean hasEdge(int v, int w) {
+    if (isOutOfRange(v, w) || !isDefined(v, w))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int getWeight(int v, int w) {
+    if (isOutOfRange(v, w) || !isDefined(v, w))
+      return -1;
+    return mat[v][w];
+  }
+
+  @Override
+  public void removeEdge(int v, int w) {
+    if (isOutOfRange(v, w))
+      return;
+    mat[v][w] = 0;
+    edgeCount--;
+    vertexEdgeCount[v]--;
+  }
+
+}
---
+++ src/test/GraphTest/GraphTest.java
@@ -0,0 +1,157 @@
+package test.GraphTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import adt.Edge;
+import adt.Interfaces.Graph;
+import adt.LinkedGraph.*;
+import adt.MatrixGraph.*;
+
+public class GraphTest {
+
+  @Test
+  public void canCreateGraphOkTest() {
+    Graph g = new LinkedGraph(0);
+  }
+
+  @Test
+  public void sizeIs0_initializedWith0Graph_OkTest() {
+    Graph g = new LinkedGraph(0);
+    assertEquals(g.size(), 0);
+  }
+
+  @Test
+  public void sizeIs1_initializedWith1Graph_OkTest() {
+    Graph g = new LinkedGraph(1);
+    assertEquals(g.size(), 1);
+  }
+
+  @Test
+  public void sizeIs2_initializedWith2Graph_OkTest() {
+    Graph g = new LinkedGraph(2);
+    assertEquals(g.size(), 2);
+  }
+
+  @Test
+  public void addUnweightedEdge_0_0_1_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+  }
+
+  @Test
+  public void addUnweightedEdge_Neg1_Neg1_1_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(-1, -1, 1);
+  }
+
+  @Test
+  public void addUnweightedEdge_matLength_matLength_1_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(g.size(), g.size(), 1);
+  }
+
+  @Test
+  public void edgeCountIs0_initializedGraph_TestOk() {
+    Graph g = new LinkedGraph(10);
+    assertEquals(g.edgeCount(), 0);
+  }
+
+  @Test
+  public void edgeCountIs1_addedOneEdge_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    assertEquals(g.edgeCount(), 1);
+  }
+
+  @Test
+  public void edgeCountIs2_addedTwoEdges_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    g.addEdge(1, 0, 1);
+    assertEquals(g.edgeCount(), 2);
+  }
+
+  @Test
+  public void edgeCountIs1_addedTwoOverridingEdges_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    g.addEdge(0, 0, 1);
+    assertEquals(g.edgeCount(), 1);
+  }
+
+  @Test
+  public void vertexEdgeCountIs0_initializedGraph_TestOk() {
+    Graph g = new LinkedGraph(10);
+    assertEquals(g.edgeCount(0), 0);
+  }
+
+  @Test
+  public void vertexEdgeCountIs1_addedOneEdge_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    assertEquals(g.edgeCount(0), 1);
+  }
+
+  @Test
+  public void hasEdgeisFalse_GraphInitialized_TestOk() {
+    Graph g = new LinkedGraph(10);
+    assertFalse(g.hasEdge(0, 0));
+  }
+
+  @Test
+  public void hasEdgeisTrue_addedOneEdge_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    assertTrue(g.hasEdge(0, 0));
+  }
+
+  @Test
+  public void hasEdgeisTrue_addedTwoEdge_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    g.addEdge(1, 2, 1);
+    assertTrue(g.hasEdge(0, 0));
+  }
+
+  @Test
+  public void getWeightIsNeg1_GraphInitialized_TestOk() {
+    Graph g = new LinkedGraph(10);
+    assertEquals(g.getWeight(0, 0), -1);
+  }
+
+  @Test
+  public void getWeightIs1_addedOneUnweightedEdge_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    assertEquals(g.getWeight(0, 0), 1);
+  }
+
+  @Test
+  public void getWeightIs5_addedOneWeightedEdge_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 5);
+    assertEquals(g.getWeight(0, 0), 5);
+  }
+
+  @Test
+  public void removeEdge_InitializedGraph_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.removeEdge(0, 0);
+    assertFalse(g.hasEdge(0, 0));
+  }
+
+  @Test
+  public void removeEdge_addedOneEdge_TestOk() {
+    Graph g = new LinkedGraph(10);
+    g.addEdge(0, 0, 1);
+    g.removeEdge(0, 0);
+    assertFalse(g.hasEdge(0, 0));
+    assertEquals(g.edgeCount(0), 0);
+    assertEquals(g.edgeCount(), 0);
+  }
+
+}
